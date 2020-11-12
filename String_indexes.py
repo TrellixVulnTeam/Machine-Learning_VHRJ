51959605a65dbcc532a1a30271114ad213c5b9f6
arnenyeck06 <74014747+arnenyeck06@users.noreply.github.com>
2020-11-11 19:43:39 -0600

    Add files via upload

---
+++ String_indexes.py
@@ -0,0 +1,17 @@
+selfish = '01234567'
+        #  01234567 Just to keep the indexes
+print(selfish[0]) 
+print(selfish[7])
+print(selfish) 
+# Start: stop
+print(selfish[0:6]) 
+
+#Start:stop:stepover It is string slicing
+print(selfish[0:8:2]) 
+
+print(selfish[1:]) 
+print(selfish[:5]) 
+print(selfish[::1]) 
+print(selfish[-7]) 
+print(selfish[::-1]) #Reverse an order
+
