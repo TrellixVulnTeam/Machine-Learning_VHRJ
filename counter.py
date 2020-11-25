6adad6515c17941c1aa818148e8568dab1769188
Arne Nyeck <nyeck2008@live.com>
2020-11-25 14:01:36 -0600

    Feat:Counter, for loops

---
+++ counter.py
@@ -0,0 +1,15 @@
+my_list = [1,2,3,4,5,6,7,8,9,10]
+counter = 0
+for item in my_list:
+    counter = counter + item
+    print(counter)
+
+    #range
+print(range(100))
+
+for number in range(0, 10):
+    print('email email list')
+
+    #range with step over
+for item in range(0, 10, 2):
+    print(item)
---
+++ for_Loops.py
@@ -0,0 +1,12 @@
+#loops, iterating
+user = {
+    'name': 'Golem',
+    'age': 5006,
+    'can_swim': False
+}
+for item in user.items():
+    print(item)
+
+for k, v in user.items():
+    print(k, v)
+
