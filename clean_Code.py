6de39109a15f0ca378131430d0d11fc1f2e6d706
Arne Nyeck <nyeck2008@live.com>
2020-12-08 14:16:31 -0600

    Tesla example, how to clean a code, methods v functions.

---
+++ clean_Code.py
@@ -0,0 +1,7 @@
+def is__even(num):
+    return num % 2 == 0
+
+print(is__even(50))
+
+     # Clean because no use of if, elif statements,
+        #return true, elif return false....
---
+++ methods_v_functions.py
@@ -0,0 +1,13 @@
+#Built in functions are color coded, or we can create with def.
+#methods are used with dot(.)notation.
+
+#docstrings are used as other types of comments within the code
+
+
+def test(a):
+    '''
+    Info: This function tests and prints param a
+    '''
+    print(a)
+
+print(test.__doc__)
